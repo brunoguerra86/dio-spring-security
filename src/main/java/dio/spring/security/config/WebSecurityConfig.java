005ee42e11ecbe285ae49f429fbc3a5cbbd25f28
brunoguerra <brunoguerra@hotmail.com>
2022-01-12 21:20:02 -0300

    Alterando gestão de usários para Banco de Dados

--- src/main/java/dio/spring/security/config/WebSecurityConfig.java
+++ src/main/java/dio/spring/security/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package dio.spring.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,12 +8,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private SecurityDatabaseService securityService;
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests()
@@ -20,19 +30,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST,"/login").permitAll()
                 .antMatchers("/managers").hasAnyRole("MANAGERS")
                 .antMatchers("/users").hasAnyRole("USERS", "MANAGERS")
-                .anyRequest().authenticated().and().formLogin();
+                .anyRequest().authenticated().and().httpBasic();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("{noop}123")
-                .roles("USERS")
-                .and()
-                .withUser("admin")
-                .password("{noop}123")
-                .roles("MANAGERS");
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                .password("{noop}123")
+//                .roles("USERS")
+//                .and()
+//                .withUser("admin")
+//                .password("{noop}123")
+//                .roles("MANAGERS");
+//    }
 
 }
